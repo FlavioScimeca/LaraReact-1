d1efe9c14f49addb9599fb11ba5f713e4082ad02
Flavio Scimeca <flavioscimeca3@gmail.com>
2023-03-03 11:30:51 +0100

    created Laravel and React, installed Tailwind CSS and Heroicons

---
+++ react/src/App.jsx
@@ -0,0 +1,12 @@
+import { BeakerIcon } from "@heroicons/react/24/solid";
+
+function App() {
+    return (
+        <div className="bg-purple-500">
+            <h2>ciao</h2>
+            <BeakerIcon className="w-5" />
+        </div>
+    );
+}
+
+export default App;
